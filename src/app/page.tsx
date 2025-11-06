802d04e6a110c1568b32d22fcd1dbbd1016361cd
Dulani Kariyawasam <hansamalidulani6678@gmail.com>
2025-11-06 09:17:02 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Appoiments from './Appointments/page'
 import AppointmentBooking from './Appointments/page'
+import DashboardPage from './dashboard/page'
 
 
 function HomePage
 () {
   return (
     <div>
-      <Appoiments/>
-      <AppointmentBooking/>
+      {/* <Appoiments/>
+      <AppointmentBooking/> */}
+      <DashboardPage/>
     </div>
   )
 }
